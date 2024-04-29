83e49e9059f863601a745db12ed0dcc60a527e2b
Anurag30112003 <anuragsharma011011@gmail.com>
2024-04-29 14:55:49 +0530

    fix: thread locking in client.

--- src/kv/KVClient.java
+++ src/kv/KVClient.java
@@ -2,24 +2,35 @@ package kv;
 
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class KVClient {
     private Socket socket;
     private PrintWriter writer;
     private BufferedReader reader;
-
+    // Synchronizing both threads so client main thread can wait for response from server.
+    private Lock lock = new ReentrantLock();
+    private Condition receivedResponse = lock.newCondition();
+  
     public KVClient(String host, int port) throws IOException {
-        // create a socket object and connect to the server.
         this.socket = new Socket(host, port);
         this.writer = new PrintWriter(socket.getOutputStream(), true);
         this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-        // start a new thread that constantly reads the responses from the server
+  
+        // thread listens for events from server. Is asynchronous in nature.
         new Thread(() -> {
             String response;
             try {
                 while ((response = reader.readLine()) != null) {
                     System.out.println(response);
+                    lock.lock();
+                    try {
+                        receivedResponse.signalAll();
+                    } finally {
+                        lock.unlock();
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -27,15 +38,24 @@ public class KVClient {
         }).start();
     }
 
-    public void sendCommand(String command) {
-        writer.println(command);
+    public void sendCommand(String command) throws InterruptedException {
+        // main thread is locked
+        lock.lock();
+        try {
+            writer.println(command);
+            // awaiting response from server
+            receivedResponse.await();
+        } finally {
+            // main thread is unlocked
+            lock.unlock();
+        }
     }
 
     public void close() throws IOException {
         socket.close();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         String host = "localhost";
         int port = 1111;
 
